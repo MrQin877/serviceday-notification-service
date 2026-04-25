b8a20a94c85dca3063a46c9a23b6c97245c9268c
MrQin877 <kuanchinzhong@gmail.com>
2026-04-26 03:22:09 +0800

    Update settings.py

--- notification_service/settings.py
+++ notification_service/settings.py
@@ -28,8 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'serviceday-local-dev-secret-key-2026')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
-
+DEBUG = 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1 localhost').split()
 
 
