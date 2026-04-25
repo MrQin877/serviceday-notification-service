3e431b1c8feee94fad89dbc2fc6ed06757b048f2
MrQin877 <kuanchinzhong@gmail.com>
2026-04-26 02:14:20 +0800

    Update settings.py

--- notification_service/settings.py
+++ notification_service/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'serviceday-local-dev-secret-key-2026'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('NOTIFICATION_SERVICE_URL', '127.0.0.1 localhost').split()
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1 localhost').split()
 
 
 # Application definition
